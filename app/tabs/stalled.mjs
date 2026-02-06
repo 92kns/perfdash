2d58c9bde8b7c27981663a43c2df2da5cbea9eea
KS <KS@local>
2026-02-06 10:22:57 -0500

    bring back long-* bugs

--- app/tabs/stalled.mjs
+++ app/tabs/stalled.mjs
@@ -1,12 +1,20 @@
 import { _ } from "util";
 import * as BugList from "buglist";
 import * as AssignedInactive from "buglists/assigned-inactive";
+import * as LongDefects from "buglists/long-defects";
+import * as LongEnhancements from "buglists/long-enhancements";
+import * as LongTasks from "buglists/long-tasks";
 import * as Stalled from "buglists/stalled";
 
 export function initUI() {
     const $content = _("#stalled-content");
 
-    const $group = BugList.newGroup($content);
+    let $group = BugList.newGroup($content);
     Stalled.init($group);
     AssignedInactive.init($group);
+
+    $group = BugList.newGroup($content);
+    LongDefects.init($group);
+    LongEnhancements.init($group);
+    LongTasks.init($group);
 }
